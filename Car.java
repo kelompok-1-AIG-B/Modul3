36e6c44f85966f5d0b79202b4003fce408c5fac2
Fifin <fifinagustiana05@gmail.com>
2025-11-02 21:00:54 +0700

    file class car

---
+++ Car.java
@@ -0,0 +1,14 @@
+class Car implements VehicleInterface, DoorInterface {
+@Override
+public void drive() {
+}
+@Override
+public void stop() {
+}
+@Override
+public void refuel() {
+}
+@Override
+public void openDoors() {
+}
+}
