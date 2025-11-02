a6aff3c19d5e2e33b24259f81988ec7f894e7d6a
ibay16 <ibaybayu4@gmail.com>
2025-11-02 21:16:15 +0700

    perbaikan class Motorcycle

---
+++ Motorcycle.java
@@ -0,0 +1,16 @@
+class Motorcycle implements VehicleInterface {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void refuel() {
+
+    }
+}
